1208c8a2b03ed813093a1d830f330a2836c04d14
Sergey Sergeev <ssergeev@amdocs.com>
2018-12-06 16:24:09 +0300

    Read data by object id

--- src/main/java/com/github/afobo/Main.java
+++ src/main/java/com/github/afobo/Main.java
@@ -2,16 +2,28 @@ package com.github.afobo;
 
 import org.hibernate.Session;
 
+import java.util.List;
+
 public class Main {
 	public static void main(String[] args) {
         fillData();
         readDataByRID();
+        readDataByOID();
     }
 
     private static void readDataByRID() {
         Session session = HibernateUtil.getSessionFactory().openSession();
         Product p1 = (Product) session.get(Product.class, 41);
         System.out.println("p1.getName() = " + p1.getName());
+    }
+
+    private static void readDataByOID() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        List list = session.createQuery("from Product where oid = 50").list();
+        for (Object o : list) {
+            Product p = (Product) o;
+            System.out.println("p.getName() = " + p.getName());
+        }
     }
 
     private static void fillData() {
