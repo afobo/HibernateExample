2450f32b0eeb088a9bf428539ae2b3b4cb194861
Sergey Sergeev <ssergeev@amdocs.com>
2018-12-06 16:37:41 +0300

    Some corrections

--- src/main/java/com/github/afobo/Main.java
+++ src/main/java/com/github/afobo/Main.java
@@ -5,11 +5,16 @@ import org.hibernate.Session;
 import java.util.List;
 
 public class Main {
-	public static void main(String[] args) {
+
+    public static final int MAX_VALUE = 99999;
+
+    public static void main(String[] args) {
         fillData();
         readDataByRID();
         readDataByOID();
-        readDataByOID_WithGIDFilter();
+        readDataByOID_WithGIDFilter(1);
+        readDataByOID_WithGIDFilter(2);
+        readDataByOID_WithGIDFilter(MAX_VALUE);
     }
 
     private static void readDataByRID() {
@@ -29,10 +34,10 @@ public class Main {
         }
     }
 
-    private static void readDataByOID_WithGIDFilter() {
-        System.out.println("=== readDataByOID_WithGIDFilter");
+    private static void readDataByOID_WithGIDFilter(int gid) {
+        System.out.println("=== readDataByOID_WithGIDFilter: " + gid);
         Session session = HibernateUtil.getSessionFactory().openSession();
-        session.enableFilter("gidFilter").setParameter("gid", 1);
+        session.enableFilter("gidFilter").setParameter("gid", gid);
         List list = session.createQuery("from Product where oid = 50").list();
         for (Object o : list) {
             Product p = (Product) o;
@@ -46,7 +51,7 @@ public class Main {
         session.createQuery("DELETE FROM Product").executeUpdate();
         session.save(new Product(40, 50, 1, 1, "Save"));
         session.save(new Product(41, 50, 2, 2, "Save1"));
-        session.save(new Product(42, 50, 3, 99999, "Save2"));
+        session.save(new Product(42, 50, 3, MAX_VALUE, "Save2"));
         session.getTransaction().commit();
     }
 }
