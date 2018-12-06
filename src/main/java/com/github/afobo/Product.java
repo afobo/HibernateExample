7937ff01f28c38c3a23f3518252f9c2a2defe6fe
Sergey Sergeev <ssergeev@amdocs.com>
2018-12-06 16:50:30 +0300

    Reformat

--- src/main/java/com/github/afobo/Product.java
+++ src/main/java/com/github/afobo/Product.java
@@ -2,7 +2,6 @@ package com.github.afobo;
 
 import org.hibernate.annotations.Filter;
 import org.hibernate.annotations.FilterDef;
-import org.hibernate.annotations.Filters;
 import org.hibernate.annotations.ParamDef;
 
 import javax.persistence.Column;
@@ -12,74 +11,70 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "PRODUCTS")
-@FilterDef(name="gidFilter", parameters={
-		@ParamDef( name="gid", type="integer" )
-})
-@Filters( {
-		@Filter(name="gidFilter", condition=":gid between from_gid and to_gid")
-} )
+@FilterDef(name = "gidFilter", parameters = {@ParamDef(name = "gid", type = "integer")})
+@Filter(name = "gidFilter", condition = ":gid between from_gid and to_gid")
 public class Product implements java.io.Serializable {
 
-	private int rid;
-	private int oid;
-	private int from_gid;
-	private int to_gid;
-	private String name;
+    private int rid;
+    private int oid;
+    private int from_gid;
+    private int to_gid;
+    private String name;
 
-	public Product() {
-	}
+    public Product() {
+    }
 
-	public Product(int rid, int oid, int from_gid, int to_gid, String name) {
-		this.rid = rid;
-		this.oid = oid;
-		this.from_gid = from_gid;
-		this.to_gid = to_gid;
-		this.name = name;
-	}
+    public Product(int rid, int oid, int from_gid, int to_gid, String name) {
+        this.rid = rid;
+        this.oid = oid;
+        this.from_gid = from_gid;
+        this.to_gid = to_gid;
+        this.name = name;
+    }
 
-	@Id
-	@Column(name = "RID", unique = true, nullable = false, precision = 5, scale = 0)
-	public int getRid() {
-		return this.rid;
-	}
+    @Id
+    @Column(name = "RID", unique = true, nullable = false, precision = 5, scale = 0)
+    public int getRid() {
+        return this.rid;
+    }
 
-	public void setRid(int rid) {
-		this.rid = rid;
-	}
+    public void setRid(int rid) {
+        this.rid = rid;
+    }
 
-	@Column(name = "OID", nullable = false, precision = 5, scale = 0)
-	public int getOid() {
-		return oid;
-	}
+    @Column(name = "OID", nullable = false, precision = 5, scale = 0)
+    public int getOid() {
+        return oid;
+    }
 
-	public void setOid(int oid) {
-		this.oid = oid;
-	}
+    public void setOid(int oid) {
+        this.oid = oid;
+    }
 
-	@Column(name = "FROM_GID", nullable = false, precision = 5, scale = 0)
-	public int getFrom_gid() {
-		return from_gid;
-	}
+    @Column(name = "FROM_GID", nullable = false, precision = 5, scale = 0)
+    public int getFrom_gid() {
+        return from_gid;
+    }
 
-	public void setFrom_gid(int from_gid) {
-		this.from_gid = from_gid;
-	}
+    public void setFrom_gid(int from_gid) {
+        this.from_gid = from_gid;
+    }
 
-	@Column(name = "TO_GID", nullable = false, precision = 5, scale = 0)
-	public int getTo_gid() {
-		return to_gid;
-	}
+    @Column(name = "TO_GID", nullable = false, precision = 5, scale = 0)
+    public int getTo_gid() {
+        return to_gid;
+    }
 
-	public void setTo_gid(int to_gid) {
-		this.to_gid = to_gid;
-	}
+    public void setTo_gid(int to_gid) {
+        this.to_gid = to_gid;
+    }
 
-	@Column(name = "NAME", nullable = false, length = 20)
-	public String getName() {
-		return this.name;
-	}
+    @Column(name = "NAME", nullable = false, length = 20)
+    public String getName() {
+        return this.name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 }
